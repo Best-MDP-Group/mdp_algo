5c595140aff5f5589a6df9d5ed66d93b48b759a2
ktnnm <99934533+ktnnm@users.noreply.github.com>
2023-09-14 08:55:08 +0800

    added json parser

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import pygame
 import grid 
 import constants
+import jsonParse
 import obstacle
 import math
 from path_finding import hamiltonian
@@ -17,7 +18,6 @@ running = True
 screen = pygame.display.set_mode((constants.SCREEN_WIDTH, constants.SCREEN_HEIGHT), pygame.RESIZABLE)
 pygame.display.set_caption("Simulation")
 
-
 # Create a Grid object
 obstacles = [obstacle.Obstacle(screen,Position(50,50, Direction.TOP),1), 
 obstacle.Obstacle(screen,Position(90,90, Direction.BOTTOM),2),
@@ -25,6 +25,13 @@ obstacle.Obstacle(screen,Position(40,180, Direction.BOTTOM),3),
 obstacle.Obstacle(screen,Position(120,150, Direction.RIGHT),4),
 obstacle.Obstacle(screen,Position(150,40, Direction.LEFT),5), 
 obstacle.Obstacle(screen,Position(190,190, Direction.LEFT),6) ]
+
+
+# parse frm JSON
+# json_str = '{"cat":"obstacles","value":{"obstacles":[{"x":7,"y":14,"id":1,"d":0},{"x":15,"y":8,"id":2,"d":6},{"x":4,"y":3,"id":3,"d":2},{"x":9,"y":7,"id":4,"d":4}],"mode":"0"}}'
+# result = jsonParse.parse_json(json_str)
+
+# obstacles = jsonParse.convert_json(screen, result)
 
 #buttons
 button_list = constants.BUTTON_LIST
