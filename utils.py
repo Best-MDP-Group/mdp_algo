f22f19eaf00d8e48b12d7f9eb8adb265bf838529
advait <advait001@e.ntu.edu.sg>
2023-10-10 16:11:09 +0800

    Update utils.py

--- utils.py
+++ utils.py
@@ -4,6 +4,7 @@ import obstacle
 import constants
 from robot.position import Position, RobotPosition
 from robot.direction import Direction
+from robot.commands import TurnCommand
 
 # parse JSON file
 def parse_json(json_str):
